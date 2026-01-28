f81b104469048858dba6edc0169f34deccd36e20
ritampaine75-debug <ritampaine75@gmail.com>
2026-01-28 12:57:49 +0530

    Fix rendering error by removing index.css import
    Removed index.css import to fix rendering error.

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // We are using App.css mostly, but if you have a reset here
-import './App.css'
+import './App.css' 
+
+// এই ফাইলে আগে 'index.css' ইম্পোর্ট করা ছিল যা এরর দিচ্ছিল, সেটা সরিয়ে দিয়েছি।
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
